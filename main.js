c6c8c7c8845f18f78af1b158c9c98c99b33975b7
Ali Nooner <alinooner@gmail.com>
2022-01-04 20:29:24 -0600

    Build createIdea function

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // global variable
 var ideas = [];
+
+var saveButton = document.getElementById('save-button');
+var titleInput = document.getElementById('title');
+var bodyInput = document.getElementById('body');
+var searchIdeas = document.getElementById('search-ideas');
+var cardBox = document.querySelector('card-box');
+
+saveButton.addEventListener('click', createIdea);
+
+function createIdea() {
+  var newIdea = new Idea(titleInput.value, bodyInput.value)
+  ideas.push(newIdea);
+}
