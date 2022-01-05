59cd34710d46b295abb39dae5fd419f5afbe82f3
Ali Nooner <alinooner@gmail.com>
2022-01-04 21:10:02 -0600

    Create makeCard function

--- main.js
+++ main.js
@@ -5,11 +5,41 @@ var saveButton = document.getElementById('save-button');
 var titleInput = document.getElementById('title');
 var bodyInput = document.getElementById('body');
 var searchIdeas = document.getElementById('search-ideas');
-var cardBox = document.querySelector('card-box');
+var cardBox = document.querySelector('.card-box');
 
 saveButton.addEventListener('click', createIdea);
 
 function createIdea() {
   var newIdea = new Idea(titleInput.value, bodyInput.value)
   ideas.push(newIdea);
+  makeCard();
 }
+
+function makeCard() {
+  cardBox.innerHTML = ``
+  for (var i=0; i<ideas.length; i++) {
+  cardBox.innerHTML += `
+  <div class="card" id=${ideas[i].id}>
+    <div class="top-bar">
+      <img class="star-active icon hidden" src="./assets/star-active.svg" alt="star-active">
+      <img class="star icon" src="./assets/star.svg" alt="star">
+      <img class="delete-active icon hidden" src="./assets/delete-active.svg" alt="delete-active">
+      <img class="delete icon" src="./assets/delete.svg" alt="delete">
+    </div>
+    <section class="idea-section">
+      <h2>${ideas[i].title}</h2>
+      <p>${ideas[i].body}</p>
+    </section>
+    <div class="bottom-bar">
+      <img class="comment icon" src="./assets/comment.svg" alt="comment">
+      <h3 class="comment">Comment</h3>
+      </div>
+    </div>`
+    }
+}
+
+// function makeCard() {
+//   cardBox.innerHTML =
+//   `<h1>HEY</h1>`
+//   console.log("Something happened");
+// }
